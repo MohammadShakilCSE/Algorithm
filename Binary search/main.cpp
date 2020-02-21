2006c45bc28092e7a66bb91b9836947e6126346f
Mohammad shakil <43646753+MohammadShakilCSE@users.noreply.github.com>
2020-02-21 23:54:51 +0600

    Add files via upload

--- Binary search/main.cpp
+++ Binary search/main.cpp
@@ -1,35 +1,25 @@
 #include<iostream>
-
 using namespace std;
-
-int binarySearch (const int A[], int length, int val) {
-
-    int low, mid, high;
-
-    low = 0;            // set initial value for low
-    high = length - 1;  // set initial value for high
-
-    /* perform binary search */
-    while (low <= high) {
-        mid = low + (high - low)/2; // update mid
-
-        if (A [mid] > val) { // search left subarray for val
-            high = mid - 1;  // update high
-        }
-        else if (A [mid] < val) { // search right subarray for val
-            low = mid + 1;        // update low
-        }
-        else {           // val = A[mid]
-            return mid;  // return index of val in A
-        }
-    }
+int binarySearch(int arr[], int p, int r, int num) {
+   if (p <= r) {
+      int mid = (p + r)/2;
+      if (arr[mid] == num)
+         return mid ;
+      if (arr[mid] > num)
+         return binarySearch(arr, p, mid-1, num);
+      if (arr[mid] < num)
+         return binarySearch(arr, mid+1, r, num);
+   }
+   return -1;
 }
-int main() {
-   int arr[] = {1, 3, 15, 18, 20, 25, 33, 36, 40,89};
+int main(void) {
+   int arr[] = {1, 3, 7, 15, 18, 20, 25, 33, 36, 40};
    int n = sizeof(arr)/ sizeof(arr[0]);
-   int num = 89;
-   int result= binarySearch(arr, n-1, num);
-   (result == -1) ? cout << "Element is not present in array"
-                   : cout << "Element is present at index " << result;
+   int num = 41;
+   int index = binarySearch (arr, 0, n-1, num);
+   if(index == -1)
+      cout<< num <<" is not present in the array";
+   else
+      cout<< num <<" is present at index "<< index <<" in the array";
    return 0;
-}
+}}
